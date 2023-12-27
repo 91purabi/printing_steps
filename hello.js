88edafe321410fc8b8bbfafcdae7e60d498fdaf8
Purabi <purbichoudhury06966@gmail.com>
2023-12-27 13:58:00 +0530

    practice practice practice

--- hello.js
+++ hello.js
@@ -1,9 +1,67 @@
-const numRows = 5;
+// const numRows = 5;
 
-for(let i = 1; i<=numRows; i++){
-    let row = " "
-    for(let j = 1; j <= i; j++){
-        row+= " *";
+// for(let i = 1; i<=numRows; i++){
+//     let row = " "
+//     for(let j = 1; j <= i; j++){
+//         row+= " *";
+//     }
+//     console.log(row);
+// }
+
+// const size = 4;
+
+// for (let i = 1; i <= size; i++){
+//     let row = "";
+
+//     for (let j=1; j <= size; j++){
+//         if((i + j) % 2 === 0){
+//             row += "* ";
+//         }else {
+//             row+= "# ";
+//         }
+//     }
+
+//     console.log(row);
+// }
+
+// const numRowsPyramid = 6;
+
+// for (let i = 1; i<= numRowsPyramid; i++){
+//     let row = " ".repeat(numRowsPyramid - i);
+
+//     for (let j = 1; j <= i; j++){
+//         row += "* ";
+//     }
+
+//     console.log(row);
+// }
+
+
+// const sizeSquare = 5;
+
+// for (let i = 1; i <= sizeSquare; i++){
+//     let row = "";
+
+//     for (let j = 1; j <= sizeSquare; j++){
+//         if(i === 1 || i === sizeSquare || j === 1 || j === sizeSquare){
+//             row += "* ";
+//         } else {
+//             row += "  ";
+//         }
+//     }
+
+//     console.log(row);
+// }
+
+
+const numRowsInverted = 5;
+
+for (let i = numRowsInverted; i >= 1; i--){
+    let row = "";
+
+    for (let j = 1; j <= i; j++){
+        row += "* ";
     }
+
     console.log(row);
 }
