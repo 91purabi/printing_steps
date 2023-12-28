ab786e7be5b0f1aa7c25a23d59aea089166a5a9d
Purabi <purbichoudhury06966@gmail.com>
2023-12-28 13:37:29 +0530

    Initial commit

--- vowels.js
+++ vowels.js
@@ -1,21 +1,54 @@
-function countVowels(str) {
-    str = str.toLowerCase();
+// function countVowels(str) {
+//     str = str.toLowerCase();
   
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
   
-    let vowelCount = 0;
+//     let vowelCount = 0;
   
-    for (let i = 0; i < str.length; i++) {
-      if (vowels.includes(str[i])) {
-        vowelCount++;
-      }
-    }
+//     for (let i = 0; i < str.length; i++) {
+//       if (vowels.includes(str[i])) {
+//         vowelCount++;
+//       }
+//     }
   
-    return vowelCount;
-  }
+//     return vowelCount;
+//   }
   
-  const inputString = "Hello, World!";
-  const result = countVowels(inputString);
+//   const inputString = "Hello, World!";
+//   const result = countVowels(inputString);
   
-  console.log(`The number of vowels in "${inputString}" is: ${result}`);
+//   console.log(`The number of vowels in "${inputString}" is: ${result}`);
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
   
+// let sum = 0;
+// for (let i = 1; i <= 10; i++){
+//     sum += i;
+// }
+
+// console.log("Sum:", sum);
+
+// function square(num) {
+//     return num * num;
+// }
+
+// let result = square(5);
+// console.log("Square of 5:", result);
+
+// let array = [3, 6, 7, 10, 55];
+
+// let max = array[0];
+// for (let num of array) {
+//     if (num > max){
+//         max = num;
+//     }
+// }
+
+// console.log("Maximum:", max);
+
+
+
+
+ 
