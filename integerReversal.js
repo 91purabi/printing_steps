931cdb60df3c1fb7516a2d8b1db55ecbc1672a9b
Purabi <purbichoudhury06966@gmail.com>
2023-12-30 22:42:15 +0530

    practice

---
+++ integerReversal.js
@@ -0,0 +1,13 @@
+// function reverseInteger(number) {
+//     const numberString = number.toString();
+//     const reversedString = numberString.split('').reverse().join('');
+//     const reversedNumber = parseInt(reversedString, 10);
+//     return reversedNumber;
+// }
+
+// const originalNumber = 12345;
+// const reversedNumber = reverseInteger(originalNumber);
+
+// console.log(`Original Number: ${originalNumber}`);
+// console.log(`Reversed Number: ${reversedNumber}`);
+
