6676929137c46c72e002a48e9c0b4fb5ecb2d9ab
Purabi <purbichoudhury06966@gmail.com>
2024-01-14 11:20:53 +0530

    practice basic

---
+++ index.js
@@ -0,0 +1,17 @@
+// let a = 5;
+// let b = 7;
+
+// let sum = a + b;
+
+// console.log(sum);
+
+let temperature = 30;
+if (temperature > 25) {
+    console.log("It's a hot day!");
+} else {
+    console.log("It's not too hot.");
+}
+
+for (let i = 0; i < 5; i++){
+    console.log(i);
+}
